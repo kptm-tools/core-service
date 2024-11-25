3a20ca8d01ffe07bacfdde7815c035dc5268a4c4
grez-lucas <lucasgrez3010@gmail.com>
2024-11-25 16:37:04 -0300

    fix(target): Update OperatorID field in mock targets

--- pkg/services/target.go
+++ pkg/services/target.go
@@ -21,22 +21,22 @@ func (s *TargetService) GetAllTargets(tenantID string) (*[]domain.Target, error)
 	targets := &[]domain.Target{
 
 		{
-			ID:        "1",
-			TenantID:  "fcde6d34-ac73-4f29-8e48-bdc5670e1d69",
-			UserID:    "74eb9201-2926-40bb-9f8c-a3a52b3b5db7",
-			Value:     "www.facebook.com",
-			Type:      domain.TargetType(domain.Domain),
-			CreatedAt: time.Now().UTC(),
-			UpdatedAt: time.Now().UTC(),
+			ID:         "1",
+			TenantID:   "fcde6d34-ac73-4f29-8e48-bdc5670e1d69",
+			OperatorID: "74eb9201-2926-40bb-9f8c-a3a52b3b5db7",
+			Value:      "www.facebook.com",
+			Type:       domain.TargetType(domain.Domain),
+			CreatedAt:  time.Now().UTC(),
+			UpdatedAt:  time.Now().UTC(),
 		},
 		{
-			ID:        "2",
-			TenantID:  "fcde6d34-ac73-4f29-8e48-bdc5670e1d69",
-			UserID:    "74eb9201-2926-40bb-9f8c-a3a52b3b5db7",
-			Value:     "www.google.com",
-			Type:      domain.TargetType(domain.Domain),
-			CreatedAt: time.Now().UTC(),
-			UpdatedAt: time.Now().UTC(),
+			ID:         "2",
+			TenantID:   "fcde6d34-ac73-4f29-8e48-bdc5670e1d69",
+			OperatorID: "74eb9201-2926-40bb-9f8c-a3a52b3b5db7",
+			Value:      "www.google.com",
+			Type:       domain.TargetType(domain.Domain),
+			CreatedAt:  time.Now().UTC(),
+			UpdatedAt:  time.Now().UTC(),
 		},
 	}
 
