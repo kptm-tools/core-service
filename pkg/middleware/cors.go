3ff027b5a8c1834813ddb252b6f08495c54db94b
grez-lucas <lucasgrez3010@gmail.com>
2024-12-02 14:04:11 -0300

    fix(cors) Update pre-flight logic to return 204 when OK

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -9,17 +9,24 @@ import (
 )
 
 var originAllowlist = config.LoadConfig().GetAllowedOrigins()
-var methodAllowlist = []string{"GET", "POST", "DELETE", "OPTIONS"}
+var methodAllowlist = []string{"GET", "POST", "OPTIONS", "OPTIONS"}
+var allowedHeaders = []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token", "Authorization"}
 
 func CheckCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		origin := r.Header.Get("Origin")
-		method := r.Header.Get("Access-Control-Request-Method")
-
-		if slices.Contains(originAllowlist, origin) && slices.Contains(methodAllowlist, method) {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", strings.Join(methodAllowlist, ", "))
+		if isPreflight(r) {
+			origin := r.Header.Get("Origin")
+			method := r.Header.Get("Access-Control-Request-Method")
+			if slices.Contains(originAllowlist, origin) && slices.Contains(methodAllowlist, method) {
+				// Preflight request (OPTIONS)
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				w.Header().Set("Access-Control-Allow-Methods", strings.Join(methodAllowlist, ", "))
+				w.Header().Set("Access-Control-Allow-Headers", strings.Join(allowedHeaders, ", "))
+				w.Header().Set("Allow", strings.Join(methodAllowlist, ", "))
+				w.WriteHeader(http.StatusNoContent) // Write the status
+				return
+			}
 		} else {
 			// Not a preflight: regular request
 			origin := r.Header.Get("Origin")
@@ -27,8 +34,13 @@ func CheckCORS(next http.Handler) http.Handler {
 				w.Header().Set("Access-Control-Allow-Origin", origin)
 			}
 		}
-
 		w.Header().Add("Vary", "Origin")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func isPreflight(r *http.Request) bool {
+	return r.Method == "OPTIONS" &&
+		r.Header.Get("Origin") != "" &&
+		r.Header.Get("Access-Control-Request-Method") != ""
 }
