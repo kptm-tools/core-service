5b5d8ce200c3018b9210403947383fd60d35a094
grez-lucas <lucasgrez3010@gmail.com>
2024-12-12 16:44:54 -0300

    feat(sample-data) Add populateDB function

--- cmd/sample-data/main.go
+++ cmd/sample-data/main.go
@@ -3,35 +3,67 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/kptm-tools/core-service/pkg/config"
+	"github.com/kptm-tools/core-service/pkg/interfaces"
 	"github.com/kptm-tools/core-service/pkg/samples"
 	"github.com/kptm-tools/core-service/pkg/services"
 	"github.com/kptm-tools/core-service/pkg/storage"
 )
 
 func main() {
-	c := config.LoadConfig()
 
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go [populate|clear]")
+		return
+	}
+
+	c := config.LoadConfig()
 	coreStore, err := storage.NewPostgreSQLStore(c.PostgreSQLCoreConnStr())
 	if err != nil {
 		log.Fatalf("Failed to create Core DB store: `%+v`", err)
 	}
 
-	// Services
-	tenantService := services.NewTenantService(coreStore)
+	command := os.Args[1]
+	switch command {
 
-	fmt.Println("Populating DB with sample data...")
+	case "populate":
+		populateDB(coreStore)
+	case "clear":
+		clearDB(coreStore)
+	default:
+		fmt.Printf("Unknown command `%s`\n", command)
+		fmt.Println("Usage: go run main.go [populate|clear]")
+	}
 
-	// Add Sample Tenants
+}
+
+func clearDB(store interfaces.IStorage) {
+	log.Println("Clearing DB...")
+	log.Println("DB cleared")
+
+}
+
+func populateDB(store interfaces.IStorage) {
+	log.Println("Populating DB with sample data...")
+
+	if err := populateTenants(store); err != nil {
+		log.Fatalf(err.Error())
+	}
+	log.Println("Tenants populated successfully")
+
+}
+
+func populateTenants(store interfaces.IStorage) error {
+	tenantService := services.NewTenantService(store)
 	sampleTenants := samples.SampleTenants()
 
 	for _, tenant := range sampleTenants {
 		_, err := tenantService.CreateTenant(&tenant)
 		if err != nil {
-			log.Printf("Error populating tenant `%+v`: `%v`\n", tenant, err)
+			return fmt.Errorf("Error populating tenant `%+v`: `%v`\n", tenant, err)
 		}
 	}
-	log.Println("Tenants added successfully")
-
+	return nil
 }
