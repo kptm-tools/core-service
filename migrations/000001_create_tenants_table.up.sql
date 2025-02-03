a544013ed32e985d7f2ca1983e9f23116334d12b
grez-lucas <lucasgrez3010@gmail.com>
2025-02-03 17:50:29 -0300

    feat(migrations) Add tenants table migrations

---
+++ migrations/000001_create_tenants_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS tenants(
+    id integer NOT NULL,
+    provider_id uuid,
+    application_id uuid,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
