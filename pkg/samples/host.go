f111b4ac41aebea306d752f5f49db882f51de0da
grez-lucas <lucasgrez3010@gmail.com>
2025-02-04 16:57:40 -0300

    fix(samples) Update env var definition

--- pkg/samples/host.go
+++ pkg/samples/host.go
@@ -10,6 +10,6 @@ func SampleHosts() []domain.Host {
 	return []domain.Host{
 		*domain.NewHost("https://www.aynitech.com", "", "11111111-0000-0000-0000-000000000000", "00000000-0000-0000-0000-111111111111", "aynitech-landing", []domain.Credential{}, []domain.Rapporteur{{Name: "Lucas", Email: "lucas@example.com", IsPrincipal: true}}),
 		*domain.NewHost("https://www.i2linked.com", "", "11111111-0000-0000-0000-000000000000", "00000000-0000-0000-0000-111111111111", "i2linked", []domain.Credential{{Username: "myuser", Password: "mypassword"}, {Username: "myuser2", Password: "mypassword2"}}, []domain.Rapporteur{}),
-		*domain.NewHost("https://www.example.com", "", c.BlueprintTenantID, "00000000-0000-0000-0000-222222222222", "example-website", []domain.Credential{}, []domain.Rapporteur{}),
+		*domain.NewHost("https://www.example.com", "", c.FusionAuth.BlueprintTenantID, "00000000-0000-0000-0000-222222222222", "example-website", []domain.Credential{}, []domain.Rapporteur{}),
 	}
 }
