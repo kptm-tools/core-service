ccdd767948cb3b436bfee7a667931374d54409cc
grez-lucas <lucasgrez3010@gmail.com>
2025-02-21 17:56:42 -0300

    refactor(storage): Remove redundant columns

--- cmd/migrations/migrations/000017_enrich_scan_vulnerabilities.down.sql
+++ cmd/migrations/migrations/000017_enrich_scan_vulnerabilities.down.sql
@@ -3,22 +3,25 @@ ALTER TABLE scan_vulnerabilities
 DROP COLUMN operating_system_id,
 DROP COLUMN service_id,
 DROP COLUMN description,
-DROP COLUMN service_access,
+DROP COLUMN access_type,
 DROP COLUMN complexity,
 DROP COLUMN privileges_required,
 DROP COLUMN likelihood,
 DROP COLUMN risk_score,
 DROP COLUMN impact_score,
-DROP COLUMN exploit,
+DROP COLUMN exploit_score,
+DROP COLUMN exploitability,
 DROP COLUMN integrity_impact,
 DROP COLUMN availability_impact,
 DROP COLUMN base_severity,
 DROP COLUMN published,
 DROP COLUMN last_updated,
 ADD COLUMN port INTEGER CHECK (port >= 0 AND port <= 65535),
+ADD COLUMN port_state port_state_enum NOT NULL,
 ADD COLUMN protocol VARCHAR(10),
 ADD COLUMN service_name VARCHAR(255),
-ADD COLUMN service_version VARCHAR(255);
+ADD COLUMN service_version VARCHAR(255),
+ADD COLUMN exploitable BOOL;
 
 DROP INDEX idx_scan_vulnerabilities_os_id;
 DROP INDEX idx_scan_vulnerabilities_service_id;
