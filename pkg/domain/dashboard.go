61f3ff65cf6a55c428993f1da49ea3c8617a3579
grez-lucas <lucasgrez3010@gmail.com>
2025-03-06 08:52:16 -0300

    feat(data-structure): Define TenantDashboardData struct

--- pkg/domain/dashboard.go
+++ pkg/domain/dashboard.go
@@ -1,6 +1,32 @@
 package domain
 
-import "github.com/kptm-tools/common/common/pkg/results/tools"
+import (
+	"time"
+
+	"github.com/kptm-tools/common/common/pkg/results/tools"
+)
+
+type TenantDashboardData struct {
+	OverallSecurityPosture           OverallSecurityPostureData
+	HostSeverityHeatMap              HostSeverityHeatMapData
+	VulnerabilityTrends              []ServiceTimePeriod
+	LastScan                         *LastScanData
+	VulnerabilitySeverityCount       tools.SeverityCounts
+	HostsWithGreatestVulnerabilities map[string]int
+}
+
+type OverallSecurityPostureData struct {
+	Score     float64
+	Variation float64
+}
+
+type LastScanData struct {
+	HostAlias                     string
+	TotalVulnerabilities          int
+	TotalVulnerabilitiesVariation int
+	SeverityCounts                tools.SeverityCounts
+	ScanDate                      time.Time
+}
 
 // HostSeverityHeatMapData is a map where keys are hostnames (strings)
 // and values are tools.SeverityCounts, representing vulnerability counts
