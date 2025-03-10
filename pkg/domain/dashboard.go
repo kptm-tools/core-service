86c4e6a86e605795b7abfca4e188dfd9827ba9db
grez-lucas <lucasgrez3010@gmail.com>
2025-03-10 12:40:54 -0300

    feat(domain): Add TimePeriodFilter enum

--- pkg/domain/dashboard.go
+++ pkg/domain/dashboard.go
@@ -6,6 +6,47 @@ import (
 	"github.com/kptm-tools/common/common/pkg/results/tools"
 )
 
+type TimePeriodFilter string
+
+const (
+	TimePeriodFilterMonth    TimePeriodFilter = "Month"
+	TimePeriodFilterQuarter  TimePeriodFilter = "Quarter"
+	TimePeriodFilterSemester TimePeriodFilter = "Semester"
+)
+
+func (t TimePeriodFilter) String() string {
+	return string(t)
+}
+
+// GetOrderedTimePeriods returns a slice of ordered time periods based on the TimePeriodFilter
+func (t TimePeriodFilter) GetOrderedTimePeriods() []string {
+	switch t {
+	case TimePeriodFilterMonth:
+		return getMonthsInOrder()
+	case TimePeriodFilterQuarter:
+		return getQuartersInOrder()
+	case TimePeriodFilterSemester:
+		return getSemestersInOrder()
+	default:
+		return getMonthsInOrder()
+	}
+}
+
+func getMonthsInOrder() []string {
+	return []string{
+		"January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December",
+	}
+}
+
+func getQuartersInOrder() []string {
+	return []string{"Q1", "Q2", "Q3", "Q4"}
+}
+
+func getSemestersInOrder() []string {
+	return []string{"Semester 1", "Semester 2"}
+}
+
 type TenantDashboardData struct {
 	OverallSecurityPosture           OverallSecurityPostureData
 	HostSeverityHeatMap              []HostAliasSeverityCountPair
