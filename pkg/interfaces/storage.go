52210750ba58c36ff2d8e895bf9eea6c1aeb7614
Cristiano Catolico <jlozano@uni.edu.pe>
2025-03-11 11:43:55 -0500

    fix: conflicts

--- pkg/interfaces/storage.go
+++ pkg/interfaces/storage.go
@@ -53,6 +53,5 @@ type IStorage interface {
 	GetCurrentHostIDFromScanSchedule(int) (int, error)
 	ScanScheduleEnableJob(string, bool, int) error
 	GetHostVulnerabilityTrends(hostID int, timePeriodFilter domain.TimePeriodFilter, severityFilters []string) ([]domain.ServiceTimePeriod, error)
-	GetRapporteursScan(id uuid.UUID) (*[]domain.Rapporteur, string)
 	GetRapporteursAndHostAliasByScanID(scanID uuid.UUID) ([]*domain.Rapporteur, string, error)
 }
