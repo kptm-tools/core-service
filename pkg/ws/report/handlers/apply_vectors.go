da43387c9aebfb07a2bc0f65200524f7300102ea
cristianoCatolico <96089538+cristianoCatolico@users.noreply.github.com>
2025-04-11 10:51:55 -0500

    feat: apply report (#118)
    * feat: apply report
    
    * fix: update ExpectedSecurityPosture
    
    * fix: removing not used comment

--- pkg/ws/report/handlers/apply_vectors.go
+++ pkg/ws/report/handlers/apply_vectors.go
@@ -1,23 +1,17 @@
 package wshandlers
 
 import (
+	"encoding/json"
+	"github.com/kptm-tools/core-service/pkg/customerrors"
+	"github.com/kptm-tools/core-service/pkg/ws/report/dto"
+	"github.com/kptm-tools/core-service/pkg/ws/report/reportutils"
 	"log/slog"
 
-	"github.com/kptm-tools/common/common/pkg/results/tools"
 	"github.com/kptm-tools/core-service/pkg/interfaces"
 	"github.com/kptm-tools/core-service/pkg/ws/common"
 )
 
 const MessageApplyVectors = "apply_vectors_request"
-
-// ApplyVectorsMessage is the payload sent in a MessageApplyVectors
-type ApplyVectorsMessage struct{}
-
-type ReportDetailsResponse struct {
-	SolvedVulnerabilities     tools.Vulnerability `json:"solved_vulnerabilities"`
-	UnattendedVulnerabilities tools.Vulnerability `json:"unattended_vulnerabilities"`
-	ExpectedSecurityPosture   float64             `json:"expected_security_posture"`
-}
 
 type ApplyVectorsHandler struct{}
 
@@ -27,5 +21,24 @@ func NewApplyVectorsHandler() *ApplyVectorsHandler {
 
 func (h *ApplyVectorsHandler) Handle(msg common.Message, client interfaces.IReportClient) error {
 	slog.Debug("Handling Apply Vectors message...")
+
+	roomID := client.GetRoomID()
+	if roomID == "" {
+		return customerrors.NewServerSideError("Client has not joined room")
+	}
+	solved, notSolved := reportutils.FilterVulnerabilitiesByStatus(client.GetHubReport().GetRoomVulnerabilities(roomID), client.GetVectorStatus())
+	reportResponse := dto.ReportDetailsResponse{
+		SolvedVulnerabilities:     solved,
+		UnattendedVulnerabilities: notSolved,
+		ExpectedSecurityPosture:   reportutils.GetGlobalCVSSScore(notSolved),
+	}
+	responseBytes, err := json.Marshal(reportResponse)
+	if err != nil {
+		slog.Error("Failed to marshal vector update response", slog.Any("error", err))
+		return customerrors.NewServerSideError("failed to marshal vector update response")
+	}
+
+	slog.Debug("Sending back response...")
+	client.GetSend() <- responseBytes
 	return nil
 }
