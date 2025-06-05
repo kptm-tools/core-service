6f48d2c717df4711c695d62a1c4bf2e7b1c550ce
grez-lucas <lucasgrez3010@gmail.com>
2025-06-05 18:03:32 -0400

    feat(mocks): Add TxManager mock

---
+++ pkg/mocks/mock_storage/tx_manager.go
@@ -0,0 +1,21 @@
+package mock_storage
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kptm-tools/core-service/pkg/interfaces"
+)
+
+type MockTxManager struct {
+	MockDoInTx func(context.Context, interfaces.TxFunc) error
+}
+
+var _ interfaces.TxManager = (*MockTxManager)(nil)
+
+func (m *MockTxManager) DoInTX(ctx context.Context, fn interfaces.TxFunc) error {
+	if m.MockDoInTx != nil {
+		return m.MockDoInTx(ctx, fn)
+	}
+	panic(fmt.Sprintf("MockTxManager: method DoInTX called but not implemented for test: %s", ctx.Value("test_name")))
+}
