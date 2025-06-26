313487cfc85c5bcc21ace4c456bb3b0dc80240b2
Lucas Grez <lucasgrez3010@gmail.com>
2025-06-26 17:53:37 -0400

    Revert "feat: schema for web vulnerability (#144)" (#145)
    This reverts commit ad1d509717578ce0bbc8e7200fd79da84e1b9eef.

--- db/sql/migrations/000026_add_wasc_in_vulnerabilities_table.down.sql
+++
@@ -1,8 +0,0 @@
--- Migration: 000026_create_web_vulnerability_details_table.down.sql
-ALTER TABLE vulnerabilities
-DROP CONSTRAINT IF EXISTS fk_vulnerabilities_wasc_details;
-
-ALTER TABLE vulnerabilities
-DROP COLUMN IF EXISTS wasc_id;
-
-DROP TABLE IF EXISTS wasc_details;
