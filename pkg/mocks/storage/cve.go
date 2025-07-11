a625580bcf42c77c84a9378cd80bf25ff9321c74
arnold <luizarnoldch@gmail.com>
2025-07-10 19:44:21 -0500

    feature: Store layer CVE details based on cveID

--- pkg/mocks/storage/cve.go
+++ pkg/mocks/storage/cve.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/kptm-tools/common/common/pkg/results/tools"
 	repository "github.com/kptm-tools/core-service/db"
 	"github.com/kptm-tools/core-service/pkg/interfaces"
@@ -12,6 +13,7 @@ import (
 
 type MockCVERepo struct {
 	MockCreateOrUpdateCVE func(ctx context.Context, vuln tools.Vulnerability) (*repository.CveDetail, error)
+	MockGetCVEDetailsByID func(ctx context.Context, cveID uuid.UUID) (*repository.CveDetail, error)
 }
 
 var _ interfaces.CVERepository = (*MockCVERepo)(nil)
@@ -21,4 +23,11 @@ func (m *MockCVERepo) CreateOrUpdateCVE(ctx context.Context, vuln tools.Vulnerab
 		return m.MockCreateOrUpdateCVE(ctx, vuln)
 	}
 	panic(fmt.Sprintf("MockCVERepo: method CreateOrUpdateCVE called but not implemented for test: %s", ctx.Value(testutil.TestNameKey)))
+}
+
+func (m *MockCVERepo) GetCVEDetailsByID(ctx context.Context, cveID uuid.UUID) (*repository.CveDetail, error) {
+	if m.MockGetCVEDetailsByID != nil {
+		return m.MockGetCVEDetailsByID(ctx, cveID)
+	}
+	panic(fmt.Sprintf("MockCVERepo: method MockGetCVEDetailsByID called but not implemented for test: %s", ctx.Value(testutil.TestNameKey)))
 }
