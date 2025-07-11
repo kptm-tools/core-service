0a27e9de7e3b0231e42df787c0c2cc2091f7ac60
arnold <luizarnoldch@gmail.com>
2025-07-10 19:23:24 -0500

    feature: Store layer Network and OS detial integration

--- db/sql/queries/cwe_detail.sql
+++ db/sql/queries/cwe_detail.sql
@@ -11,3 +11,18 @@ INSERT INTO cwe_details (
   description = EXCLUDED.description,
   last_updated = EXCLUDED.last_updated
 RETURNING *;
+
+-- name: GetCWEDetailWithMitigationsByID :many 
+SELECT
+  cd.cwe_id,
+  cd.title,
+  cd.description,
+  cm.id AS mitigation_id,
+  cm.mitigation_id AS mitigation_code,
+  cm.phase,
+  cm.description AS mitigation_description,
+  cm.effectiveness,
+  cm.effectiveness_notes
+FROM cwe_details cd
+LEFT JOIN cwe_mitigations cm ON cd.cwe_id = cm.cwe_id
+WHERE cd.cwe_id = $1;
