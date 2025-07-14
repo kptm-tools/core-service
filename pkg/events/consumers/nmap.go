be62ecdc3db33980f0e3c791b3299318481cf453
Cristiano Catolico <jlozano@uni.edu.pe>
2025-07-14 06:34:34 -0500

    new test controller consumer and test service

--- pkg/events/consumers/nmap.go
+++ pkg/events/consumers/nmap.go
@@ -3,6 +3,7 @@ package consumers
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 	"runtime/debug"
@@ -41,109 +42,118 @@ func (h *NmapHandler) HandleMessage(msg *nats.Msg) {
 			}
 		}()
 		slog.Info("Received NmapEvent")
+
 		ctx, cancel := context.WithTimeout(context.Background(), 900*time.Second)
 		defer cancel()
 
-		// 1. Parse payload
-		var evt events.ToolResultEvent
-		if err := json.Unmarshal(msg.Data, &evt); err != nil {
-			slog.Error("Failed to unmarshal ToolResultEvent", slog.Any("error", err))
-			return
-		}
-
-		// 2. Validate contents
-		if evt.ToolResult.Tool != enums.ToolNmap {
-			slog.Error("Invalid toolName for NmapEvent",
-				slog.String("tool_name", string(evt.ToolResult.Tool)))
-			return
-		}
-		scan, errScan := h.scanService.GetScanByID(ctx, evt.ScanID)
-		if errScan != nil {
-			slog.Error("Failed to get Scan", slog.String("scan_id", evt.ScanID.String()))
-			return
-		}
-		if scan.IsFailedOrCancelled() {
-			slog.Error("Error inserting ScanResult to DB because of Scan Status",
-				slog.String("scan_id", evt.ScanID.String()),
-				slog.String("tool_name", string(evt.ToolResult.Tool)),
-				slog.Any("current_status ", scan.Status),
-			)
-			return
-		}
-		scanResult := domain.NewScanResult(evt.ScanID, evt.ToolResult)
-
-		// 3.1 Only store ToolResult, not Vulnerabilities
-		if err := h.scanService.InsertScanResult(ctx, *scanResult); err != nil {
-			slog.Error("Error inserting ScanResult to DB",
-				slog.String("scan_id", evt.ScanID.String()),
-				slog.String("tool_name", string(evt.ToolResult.Tool)),
-				slog.Any("error", err),
-			)
-
-			// Mark the scan as failed
-			if err := h.scanService.MarkScanAsFailed(ctx, evt.ScanID); err != nil {
-				slog.Error("Error marking scan as failed",
-					slog.String("scan_id", evt.ScanID.String()),
-					slog.Any("error", err))
-			}
-
-			slog.Debug("Scan marked as failed successfully", slog.String("scan_id", evt.ScanID.String()))
-
-			return
-		}
-		slog.Debug("Nmap ToolResult saved successfully")
-
-		// 2.1 Check for errors in the result
-		if evt.ToolResult.Err != nil {
-			slog.Warn("ToolResult contains an error, only storing ToolResult",
-				slog.String("scan_id", evt.ScanID.String()),
-				slog.String("tool_name", string(evt.ToolResult.Tool)),
-				slog.Any("error", evt.ToolResult.Err))
-
-			if err := h.scanService.MarkScanAsFailed(ctx, evt.ScanID); err != nil {
-				slog.Error("Error marking scan as failed",
-					slog.String("scan_id", evt.ScanID.String()),
-					slog.Any("error", err))
-				return
-			}
-			slog.Debug("Scan marked as failed successfully", slog.String("scan_id", evt.ScanID.String()))
-			return
-		}
-
-		// 3.1 Parse the vulnerabilities from the result
-		// 3.1.1 Unmarshal the result to a tools.NmapResult variable
-		var nr tools.NmapResult
-		resultPtr, ok := scanResult.Result.Result.(*tools.NmapResult)
-		if !ok || resultPtr == nil {
-			slog.Error(
-				"Failed to assert nmap result type", // Static, searchable message
-				"scan_id", scan.ID.String(),         // Structured context
-				"expected_type", "*tools.NmapResult",
-				"actual_type", fmt.Sprintf("%T", scanResult.Result.Result),
-			)
-		}
-		nr = *resultPtr
-
-		// 3.2 Pass the nmap result to the VulnService InsertNetworkOSVulnerability method
-
-		// 3.2 Begin DB transaction to store ToolResult and Vulnerabilities
-		if err := h.vulnService.CreateNetworkOSVulnerabilities(ctx, scan.ID, nr); err != nil {
-			slog.Error(
-				"Error inserting VulnerabilityResult to DB",
-				slog.String("scan_id", evt.ScanID.String()),
-				slog.String("tool_name", string(evt.ToolResult.Tool)),
-				slog.Any("error", err))
-
-			// Mark the scan as failed
-			if err := h.scanService.MarkScanAsFailed(ctx, evt.ScanID); err != nil {
-				slog.Error("Error marking scan as failed",
-					slog.String("scan_id", evt.ScanID.String()),
-					slog.Any("error", err))
-				return
-			}
-			return
+		err := h.processNmapEvent(ctx, msg.Data)
+		if err != nil {
+			slog.Error("Error processing NmapEvent", "error", err)
 		}
 
 		slog.Debug("NmapEvent handled successfully")
 	}(msg)
+}
+
+func (h *NmapHandler) processNmapEvent(ctx context.Context, data []byte) error {
+	// 1. Parse payload
+	var evt events.ToolResultEvent
+	if err := json.Unmarshal(data, &evt); err != nil {
+		slog.Error("Failed to unmarshal ToolResultEvent", slog.Any("error", err))
+		return err
+	}
+
+	// 2. Validate contents
+	if evt.ToolResult.Tool != enums.ToolNmap {
+		slog.Error("Invalid toolName for NmapEvent",
+			slog.String("tool_name", string(evt.ToolResult.Tool)))
+		return errors.New("invalid toolName for NmapEvent")
+	}
+	scan, errScan := h.scanService.GetScanByID(ctx, evt.ScanID)
+	if errScan != nil {
+		slog.Error("Failed to get Scan", slog.String("scan_id", evt.ScanID.String()))
+		return errScan
+	}
+	if scan.IsFailedOrCancelled() {
+		slog.Error("Error inserting ScanResult to DB because of Scan Status",
+			slog.String("scan_id", evt.ScanID.String()),
+			slog.String("tool_name", string(evt.ToolResult.Tool)),
+			slog.Any("current_status ", scan.Status),
+		)
+		return errors.New("error inserting ScanResult to DB because of Scan Status")
+	}
+	scanResult := domain.NewScanResult(evt.ScanID, evt.ToolResult)
+
+	// 3.1 Only store ToolResult, not Vulnerabilities
+	if err := h.scanService.InsertScanResult(ctx, *scanResult); err != nil {
+		slog.Error("Error inserting ScanResult to DB",
+			slog.String("scan_id", evt.ScanID.String()),
+			slog.String("tool_name", string(evt.ToolResult.Tool)),
+			slog.Any("error", err),
+		)
+
+		// Mark the scan as failed
+		if err := h.scanService.MarkScanAsFailed(ctx, evt.ScanID); err != nil {
+			slog.Error("Error marking scan as failed",
+				slog.String("scan_id", evt.ScanID.String()),
+				slog.Any("error", err))
+		}
+
+		slog.Debug("Scan marked as failed successfully", slog.String("scan_id", evt.ScanID.String()))
+
+		return err
+	}
+	slog.Debug("Nmap ToolResult saved successfully")
+
+	// 2.1 Check for errors in the result
+	if evt.ToolResult.Err != nil {
+		slog.Warn("ToolResult contains an error, only storing ToolResult",
+			slog.String("scan_id", evt.ScanID.String()),
+			slog.String("tool_name", string(evt.ToolResult.Tool)),
+			slog.Any("error", evt.ToolResult.Err))
+
+		if err := h.scanService.MarkScanAsFailed(ctx, evt.ScanID); err != nil {
+			slog.Error("Error marking scan as failed",
+				slog.String("scan_id", evt.ScanID.String()),
+				slog.Any("error", err))
+			return err
+		}
+		slog.Debug("Scan marked as failed successfully", slog.String("scan_id", evt.ScanID.String()))
+		return evt.ToolResult.Err
+	}
+
+	// 3.1 Parse the vulnerabilities from the result
+	// 3.1.1 Unmarshal the result to a tools.NmapResult variable
+	var nr tools.NmapResult
+	resultPtr, ok := scanResult.Result.Result.(*tools.NmapResult)
+	if !ok || resultPtr == nil {
+		slog.Error(
+			"Failed to assert nmap result type", // Static, searchable message
+			"scan_id", scan.ID.String(),         // Structured context
+			"expected_type", "*tools.NmapResult",
+			"actual_type", fmt.Sprintf("%T", scanResult.Result.Result),
+		)
+	}
+	nr = *resultPtr
+
+	// 3.2 Pass the nmap result to the VulnService InsertNetworkOSVulnerability method
+
+	// 3.2 Begin DB transaction to store ToolResult and Vulnerabilities
+	if err := h.vulnService.CreateNetworkOSVulnerabilities(ctx, scan.ID, nr); err != nil {
+		slog.Error(
+			"Error inserting VulnerabilityResult to DB",
+			slog.String("scan_id", evt.ScanID.String()),
+			slog.String("tool_name", string(evt.ToolResult.Tool)),
+			slog.Any("error", err))
+
+		// Mark the scan as failed
+		if err := h.scanService.MarkScanAsFailed(ctx, evt.ScanID); err != nil {
+			slog.Error("Error marking scan as failed",
+				slog.String("scan_id", evt.ScanID.String()),
+				slog.Any("error", err))
+			return err
+		}
+		return err
+	}
+	return nil
 }
