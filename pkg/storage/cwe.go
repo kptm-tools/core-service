d5f5b374dfde13419a79c9b8bb45b720bed554a8
Jose Lozano <whi_jlozano@niubiz.com.pe>
2025-07-19 16:54:40 -0500

    fix: rebase

--- pkg/storage/cwe.go
+++ pkg/storage/cwe.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/kptm-tools/common/common/pkg/results/tools"
 	repository "github.com/kptm-tools/core-service/db"
-	"github.com/kptm-tools/core-service/pkg/domain"
 	"github.com/kptm-tools/core-service/pkg/interfaces"
 )
 
