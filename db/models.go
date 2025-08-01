6bd88468baff3a769a6c03610bd6d3298abbf90b
grez-lucas <lucasgrez3010@gmail.com>
2025-08-01 12:48:50 -0400

    fix: Update vulnerability queries with deprecated WASC_ID column

--- db/models.go
+++ db/models.go
@@ -430,7 +430,6 @@ type Vulnerability struct {
 	CreatedAt      sql.NullTime          `json:"created_at"`
 	UpdatedAt      sql.NullTime          `json:"updated_at"`
 	CweID          sql.NullString        `json:"cwe_id"`
-	WascID         sql.NullString        `json:"wasc_id"`
 }
 
 type WebVulnerability struct {
